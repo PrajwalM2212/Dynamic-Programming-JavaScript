4a98c645956979f373126209d172f202432e8acb
PrajwalM2212 <prajwalmmath@gmail.com>
2018-09-09 11:02:10 +0530

    refactoring

--- min_one.js
+++ min_one.js
@@ -43,40 +43,7 @@ function minOne(n, cache) {
 
 }
 
-// bottom-up
-function minOneBottomUp(n) {
-
-    let cache = [];
-    // base condition
-    cache[1] = 0;
-
-    for (i = 2; i <= n; i++) {
-
-        // initialize a , b and c to some very large numbers
-        let a = 1000, b = 1000, c = 1000;
-
-        // one step from i -> i-1
-        a = 1 + cache[i - 1];
-
-        // one step from i -> i/2 if i is divisible by 2
-        if (i % 2 === 0) {
-            b = 1 + cache[i / 2];
-        }
-
-        // one step from i -> i/3 if i is divisible by 3
-        if (i % 3 === 0) {
-            c = 1 + cache[i / 3];
-        }
-
-        // Store the minimum number of steps to reach i
-        cache[i] = Math.min(a, b, c);
-    }
-
-    // return the number minimum number of steps to reach n
-    return cache[n];
-}
 
 
 let cache = [];
 console.log(minOne(1000, cache));
-console.log(minOneBottomUp(1000));
